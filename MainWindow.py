d0c59d32cee774b8087abf674537b6fff690b90b
Justin Webster <webster.c.justin@gmail.com>
2026-09-25 20:22:56 -0600

    Build and Add MainWindow class .py file

---
+++ MainWindow.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+class MainWindow(tk.Tk()):
+    
+    def __init__(self): 
+       super().__init__(self, title, size, elements) 
+       self.title(title)
+       self.geometry(f"{size[0]}x{size[1]}")
+       self.minsize(size[0], size[1])
+       self.title = title
+       
+       #initialize ui widgets here
+        
+
+       
+       
+       self.mainloop()
+        
