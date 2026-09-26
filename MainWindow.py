c2d481eef7d59b28e83f2f2f5d2bf1f86712524f
Justin Webster <webster.c.justin@gmail.com>
2026-09-26 00:31:06 -0600

    Add MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,17 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
+import UserInputFrame
+import FileOutputFrame
 
-class MainWindow(tk.Tk()):
+class MainWindow(tk.Tk):
     
-    def __init__(self): 
-       super().__init__(self, title, size, elements) 
+    def __init__(self, title, size, controller): 
+       super().__init__() 
        self.title(title)
        self.geometry(f"{size[0]}x{size[1]}")
        self.minsize(size[0], size[1])
        self.title = title
-       
+       self.controller = controller
+
+       #Disable resizing
+       self.resizable(False, False)
        #initialize ui widgets here
         
+       self.userInputFrame = UserInputFrame.UserInputFrame(self) 
+       self.FileOutputFrame = FileOutputFrame.FileOutputFrame(self)
+       
 
        
        
